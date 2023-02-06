6e1816b1114f1945185a4b0c1aede53131f7b27e
HP <adiibrahimov@gmail.com>
2023-02-06 20:37:41 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Triangle triangle=new Triangle(4,5,6);
+        System.out.println(triangle.getA()+triangle.getB()+triangle.getC());
+    }
+}
